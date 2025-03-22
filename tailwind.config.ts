5c2ce99df8753e97590ddcd915456aee0ab41a79
Sergio Sancho <sergiosancho@gmail.com>
2025-03-22 18:29:02 +0100

    fix: update darkMode config in tailwind to fix type error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: ["class", "[data-theme='dark']"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
